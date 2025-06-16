17f651dacae2afb73bd753125a74e0663e36a065
aldo3241 <aldominotaur123@gmail.com>
2025-06-16 12:41:04 +0700

    add sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, Warehouse, BookOpenCheck, BookCheck, ScrollText, NotebookPen  } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -12,6 +12,26 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Barang & Jasa',
+        href: '/dashboard',
+        icon: Warehouse,
+    },
+    {
+        title: 'Jurnal Pekerjaan',
+        href: '/dashboard',
+        icon: BookCheck,
+    },
+    {
+        title: 'Keuangan',
+        href: '/dashboard',
+        icon: ScrollText,
+    },
+    {
+        title: 'Presensi',
+        href: '/dashboard',
+        icon: NotebookPen,
     },
 ];
 
