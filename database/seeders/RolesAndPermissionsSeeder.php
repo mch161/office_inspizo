9c4e77aaf95befe84430cb20e99850c799b8375f
Rellzy27 <alienbego385@gmail.com>
2025-06-20 14:17:35 +0700

    add migrations

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -13,15 +13,6 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run()
     {
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-
-        // create permissions
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
-
-        // create roles and assign created permissions
-
+        
     }
 }
