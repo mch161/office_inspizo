0e4b1109a590a4fcab7cf57400ee526f17a63344
Rellzy27 <alienbego385@gmail.com>
2025-06-24 17:34:17 +0700

    new datatable

--- resources/views/karyawan/jurnal.blade.php
+++ resources/views/karyawan/jurnal.blade.php
@@ -27,52 +27,24 @@
     <x-adminlte-button label="Tambahkan Jurnal" class="float-right mb-2 bg-blue" data-toggle="modal"
         data-target="#modalPurple" />
 
-    {{-- Setup data for datatables --}}
-
-    @php
-        $heads = [
-            'ID',
-            'Name',
-            ['label' => 'Phone', 'width' => 40],
-            ['label' => 'Actions', 'no-export' => true, 'width' => 5],
-        ];
-
-        $btnEdit = '<button class="btn btn-xs btn-default text-primary mx-1 " title="Edit">
-                <i class="fa fa-lg fa-fw fa-pen"></i>
-            </button>';
-        $btnDelete = '<button class="btn btn-xs btn-default text-danger mx-1 " title="Delete">
-                  <i class="fa fa-lg fa-fw fa-trash"></i>
-              </button>';
-        $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 " title="Details">
-                   <i class="fa fa-lg fa-fw fa-eye"></i>
-               </button>';
-
-        $config = [
-            'data' => [
-                [22, 'John Bender', '+02 (123) 123456789', '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>'],
-                [
-                    19,
-                    'Sophia Clemens',
-                    '+99 (987) 987654321',
-                    '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>',
-                ],
-                [3, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>' . $btnEdit . $btnDelete . $btnDetails . '</nobr>'],
-            ],
-            'order' => [[1, 'asc']],
-            'columns' => [null, null, null, ['orderable' => true]],
-        ];
-    @endphp
-
-    {{-- Minimal example / fill data using the component slot --}}
-    <x-adminlte-datatable id="table1" :heads="$heads" head-theme="dark">
-        @foreach ($config['data'] as $row)
-            <tr>
-                @foreach ($row as $cell)
-                    <td>{!! $cell !!}</td>
-                @endforeach
-            </tr>
-        @endforeach
-    </x-adminlte-datatable>
+<table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
 
     {{-- Compressed with style options / fill data using the plugin config --}}
     {{-- <x-adminlte-datatable id="table2" :heads="$heads" head-theme="dark" :config="$config"
