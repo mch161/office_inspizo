b1eaafe2c76742c40b30a01f135b892ce8b9c740
Rellzy27 <alienbego385@gmail.com>
2025-06-25 10:02:43 +0700

    create jurnal migration

---
+++ database/migrations/2025_06_25_025357_create_jurnal_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('jurnal', function (Blueprint $table) {
+            $table->id('kd_jurnal');
+            $table->integer('kd_user');
+            $table->string('tanggal', 20);
+            $table->string('jam', 20);
+            $table->text('isi_jurnal');
+            $table->string('dibuat_oleh', 50);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('jurnal');
+    }
+};
