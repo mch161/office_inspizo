f6feea46961ce3e793ed296d5a33f101b3c2e7a0
aldo3241 <aldominotaur123@gmail.com>
2025-07-08 09:20:31 +0700

    Adding new input form

--- resources/views/karyawan/keuangan/keuangan.blade.php
+++ resources/views/karyawan/keuangan/keuangan.blade.php
@@ -3,10 +3,14 @@
 @section('title', 'Keuangan')
 
 @section('content_header')
-    <h1>Keuangan</h1>
-    <div class="d-flex justify-content-end">
-        <x-adminlte-button label="Tambahkan" class="mb-2 bg-blue" data-toggle="modal" data-target="#modalTambah" />
-    </div>
+<h1>Keuangan</h1>
+<div class="d-flex justify-content-end">
+    <x-adminlte-button label="Tambahkan Pengeluaran" class="mb-2 bg-blue" data-toggle="modal"
+        data-target="#modalKeluar" />
+
+    <x-adminlte-button label="Tambahkan Pemasukan" class="mb-2 bg-blue" data-toggle="modal"
+        data-target="#modalTambah" />
+</div>
 
 @stop
 
@@ -15,95 +19,147 @@
 @endsection
 
 @section('content')
-    <x-adminlte-modal id="modalTambah" title="Tambahkan Histori Keuangan" theme="success" icon="fas fa-clipboard" size='lg'>
-        <form action="{{ route('keuangan.store') }}" method="POST" id="keuanganForm">
-            @csrf
-            {{-- Date picker --}}
-            @php
-                $config = ['format' => 'YYYY-MM-DD'];
-            @endphp
-            <x-adminlte-input-date name="tanggal" :config="$config" placeholder="Pilih tanggal..." label="Tanggal"
-                igroup-size="md" required>
-                <x-slot name="appendSlot">
-                    <div class="input-group-text bg-dark">
-                        <i class="fas fa-calendar-day"></i>
-                    </div>
-                </x-slot>
-            </x-adminlte-input-date>
-
-            <x-adminlte-input name="jumlah pemasukan" label="Jumlah" placeholder="Masukkan jumlah..." igroup-size="md" required>
-                <x-slot name="prependSlot">
-                    <div class="input-group-text">
-                        <i class="fa-solid fa-money-bill-trend-up"></i>
-                    </div>
-                </x-slot>
-            </x-adminlte-input>
-
-            <x-adminlte-select name="kotak" label="Kotak">
-                <x-adminlte-options :options="$kotak->pluck('nama', 'kd_kotak')->toArray()" empty-option="Select an option..." />
-            </x-adminlte-select>
-
-                        <x-adminlte-select name="kategori" label="katgori">
-                <x-adminlte-options :options="$kategori->pluck('nama', 'kd_kategori')->toArray()" empty-option="Select an option..." />
-            </x-adminlte-select>
-
-            <x-adminlte-textarea name="isi_jurnal" label="Keterangan" rows=5 igroup-size="sm"
-                placeholder="Tuliskan isi jurnal di sini..." required>
-                <x-slot name="prependSlot">
-                    <div class="input-group-text bg-dark">
-                        <i class="fas fa-lg fa-file-alt text-warning"></i>
-                    </div>
-                </x-slot>
-            </x-adminlte-textarea>
-
-            <x-slot name="footerSlot">
-                <x-adminlte-button theme="success" label="Simpan" type="submit" form="keuanganForm" />
-                <x-adminlte-button label="Batal" data-dismiss="modal" theme="danger" />
+<x-adminlte-modal id="modalTambah" title="Tambahkan Histori Keuangan" theme="success" icon="fas fa-clipboard" size='lg'>
+    <form action="{{ route('keuangan.store') }}" method="POST" id="modalTambah">
+        @csrf
+        {{-- Date picker --}}
+        @php
+            $config = ['format' => 'YYYY-MM-DD'];
+        @endphp
+        <x-adminlte-input-date name="tanggal" :config="$config" placeholder="Pilih tanggal..." label="Tanggal"
+            igroup-size="md" required>
+            <x-slot name="appendSlot">
+                <div class="input-group-text bg-dark">
+                    <i class="fas fa-calendar-day"></i>
+                </div>
             </x-slot>
-        </form>
-    </x-adminlte-modal>
-    <table id="KeuanganTable" class="table table-bordered table-striped">
-        <thead>
-            <tr class="table-primary">
-                <th width="5%">No</th>
-                <th>Jenis</th>
-                <th>Kotak</th>
-                <th>Kategori</th>
-                <th>Keterangan</th>
-                <th>Tanggal</th>
-                <th>Status</th>
-                <th width="150px" rigth>Action</th>
+        </x-adminlte-input-date>
+
+        <x-adminlte-input name="masuk" label="Uang Masuk" placeholder="Masukkan jumlah..." igroup-size="md" required>
+            <x-slot name="prependSlot">
+                <div class="input-group-text">
+                    <i class="fa-solid fa-money-bill-trend-up"></i>
+                </div>
+            </x-slot>
+        </x-adminlte-input>
+
+        <x-adminlte-select name="kotak" label="Kotak">
+            <x-adminlte-options :options="$kotak->pluck('nama', 'kd_kotak')->toArray()"
+                empty-option="Select an option..." />
+        </x-adminlte-select>
+
+        <x-adminlte-select name="kategori" label="katgori">
+            <x-adminlte-options :options="$kategori->pluck('nama', 'kd_kategori')->toArray()"
+                empty-option="Select an option..." />
+        </x-adminlte-select>
+
+        <x-adminlte-textarea name="isi_jurnal" label="Keterangan" rows=5 igroup-size="sm"
+            placeholder="Tuliskan isi jurnal di sini..." required>
+            <x-slot name="prependSlot">
+                <div class="input-group-text bg-dark">
+                    <i class="fas fa-lg fa-file-alt text-warning"></i>
+                </div>
+            </x-slot>
+        </x-adminlte-textarea>
+
+        <x-slot name="footerSlot">
+            <x-adminlte-button theme="success" label="Simpan" type="submit" form="keuanganForm" />
+            <x-adminlte-button label="Batal" data-dismiss="modal" theme="danger" />
+        </x-slot>
+    </form>
+</x-adminlte-modal>
+<x-adminlte-modal id="modalKeluar" title="Tambahkan Histori Keuangan" theme="success" icon="fas fa-clipboard" size='lg'>
+    <form action="{{ route('keuangan.store') }}" method="POST" id="ModalKeluar">
+        @csrf
+        {{-- Date picker --}}
+        @php
+            $config = ['format' => 'YYYY-MM-DD'];
+        @endphp
+        <x-adminlte-input-date name="tanggal" :config="$config" placeholder="Pilih tanggal..." label="Tanggal"
+            igroup-size="md" required>
+            <x-slot name="appendSlot">
+                <div class="input-group-text bg-dark">
+                    <i class="fas fa-calendar-day"></i>
+                </div>
+            </x-slot>
+        </x-adminlte-input-date>
+
+        <x-adminlte-input name="masuk" label="Uang Keluar" placeholder="Masukkan jumlah..." igroup-size="md" required>
+            <x-slot name="prependSlot">
+                <div class="input-group-text">
+                    <i class="fa-solid fa-money-bill-trend-up"></i>
+                </div>
+            </x-slot>
+        </x-adminlte-input>
+
+        <x-adminlte-select name="kotak" label="Kotak">
+            <x-adminlte-options :options="$kotak->pluck('nama', 'kd_kotak')->toArray()"
+                empty-option="Select an option..." />
+        </x-adminlte-select>
+
+        <x-adminlte-select name="kategori" label="katgori">
+            <x-adminlte-options :options="$kategori->pluck('nama', 'kd_kategori')->toArray()"
+                empty-option="Select an option..." />
+        </x-adminlte-select>
+
+        <x-adminlte-textarea name="isi_jurnal" label="Keterangan" rows=5 igroup-size="sm"
+            placeholder="Tuliskan isi jurnal di sini..." required>
+            <x-slot name="prependSlot">
+                <div class="input-group-text bg-dark">
+                    <i class="fas fa-lg fa-file-alt text-warning"></i>
+                </div>
+            </x-slot>
+        </x-adminlte-textarea>
+
+        <x-slot name="footerSlot">
+            <x-adminlte-button theme="success" label="Simpan" type="submit" form="keuanganForm" />
+            <x-adminlte-button label="Batal" data-dismiss="modal" theme="danger" />
+        </x-slot>
+    </form>
+</x-adminlte-modal>
+
+<table id="KeuanganTable" class="table table-bordered table-striped">
+    <thead>
+        <tr class="table-primary">
+            <th width="5%">No</th>
+            <th>Jenis</th>
+            <th>Kotak</th>
+            <th>Kategori</th>
+            <th>Keterangan</th>
+            <th>Tanggal</th>
+            <th>Status</th>
+            <th width="150px" rigth>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($keuangans as $keuangan)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $keuangan->jenis }}</td>
+                <td>{{ $keuangan->kotak->nama }}</td>
+                <td>{{ $keuangan->kategori }}</td>
+                <td>{{ $keuangan->keterangan }}</td>
+                <td>{{ $keuangan->created_at }}</td>
+                <td>{{ $keuangan->status }}</td>
+                <td>
+                    <form action="{{ route('keuangan.destroy', $keuangan->kd_keuangan) }}" method="POST"
+                        style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm tombol-hapus">
+                            Hapus
+                        </button>
+                    </form>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($keuangans as $keuangan)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $keuangan->jenis }}</td>
-                    <td>{{ $keuangan->kotak->nama }}</td>
-                    <td>{{ $keuangan->kategori }}</td>
-                    <td>{{ $keuangan->keterangan }}</td>
-                    <td>{{ $keuangan->created_at }}</td>
-                    <td>{{ $keuangan->status }}</td>
-                    <td>
-                        <form action="{{ route('keuangan.destroy', $keuangan->kd_keuangan) }}" method="POST"
-                            style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm tombol-hapus">
-                                Hapus
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+        @endforeach
+    </tbody>
+</table>
 @stop
 
 @section('js')
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#KeuanganTable').DataTable({
                 scrollX: true
             });
@@ -127,24 +183,24 @@
                 text: '{{ session('success') }}',
             })
         @endif
-        @if (session()->has('error'))
+            @if (session()->has('error'))
 
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-            });
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                });
 
-            Toast.fire({
-                icon: 'error',
-                text: '{{ session('error') }}',
-            })
-        @endif
+                Toast.fire({
+                    icon: 'error',
+                    text: '{{ session('error') }}',
+                })
+            @endif
     </script>
 @endsection
