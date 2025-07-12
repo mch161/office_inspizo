fd24174d790a098dc88bfa54c3ea6294739c932e
aldo3241 <aldominotaur123@gmail.com>
2025-07-12 09:20:02 +0700

    changing card size to be fixed size and change

---
+++ resources/views/karyawan/barang/stok.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Barang')
+
+@section('content_header')
+<h1>Barang</h1>
+@stop
+
+@section('plugins.Sweetalert2', true)
+
+
+@section('css')
+<style>
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+</style>
+
+@endsection
