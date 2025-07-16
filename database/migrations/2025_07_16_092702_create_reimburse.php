55f6570b76219230d02ab628158c201440c2e31a
aldo3241 <aldominotaur123@gmail.com>
2025-07-16 10:07:27 +0700

    add reimburse migration

---
+++ database/migrations/2025_07_16_092702_create_reimburse.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reimburse', function (Blueprint $table) {
+            $table->id("kd_reimburse");
+            $table->integer('kd_karyawan');
+            $table->string('tanggal', 20);
+            $table->string('jam', 20);
+            $table->string('foto', 255);
+            $table->text('keterangan');
+            $table->string('dibuat_oleh', 50);
+            $table->timestamps();
+
+            $table->foreign('kd_karyawan')->references('kd_karyawan')->on('karyawan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reimburse');
+    }
+};
