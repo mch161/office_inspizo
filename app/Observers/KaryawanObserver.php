f389facb320d242ae2f72c079a37756d2647584e
aldo3241 <aldominotaur123@gmail.com>
2025-07-17 12:32:11 +0700

    obeserver

--- app/Observers/KaryawanObserver.php
+++ app/Observers/KaryawanObserver.php
@@ -2,8 +2,12 @@
 
 namespace App\Observers;
 
+use App\Models\Barang;
 use App\Models\Jurnal;
 use App\Models\Karyawan;
+use App\Models\Keuangan;
+use App\Models\Reimburse;
+use App\Models\Stok;
 
 class KaryawanObserver
 {
@@ -22,6 +26,16 @@ class KaryawanObserver
     {
         if ($karyawan->isDirty('nama')) {
             Jurnal::where('kd_karyawan', $karyawan->kd_karyawan)
+                ->update(['dibuat_oleh' => $karyawan->nama]);
+            Keuangan::where('kd_karyawan', $karyawan->kd_karyawan)
+                ->update(['dibuat_oleh' => $karyawan->nama]);
+            Barang::where('kd_karyawan', $karyawan->kd_karyawan)
+                ->update(['dibuat_oleh' => $karyawan->nama]);
+            Stok::where('kd_karyawan', $karyawan->kd_karyawan)
+                ->update(['dibuat_oleh' => $karyawan->nama]);
+            Reimburse::where('kd_karyawan', $karyawan->kd_karyawan)
+                ->update(['dibuat_oleh' => $karyawan->nama]);
+            Karyawan::where('kd_karyawan', $karyawan->kd_karyawan)
                 ->update(['dibuat_oleh' => $karyawan->nama]);
         }
     }
